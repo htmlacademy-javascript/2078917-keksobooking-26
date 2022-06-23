0d11c48dc446e85c76a7d6efb3cc8cfbbb8ac8c6
Keks <keks@htmlacademy.ru>
2022-06-23 17:35:44 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+import './modules/data.js';
+import './modules/form-validation.js';
+import './modules/form-submit.js';
+import './modules/map-loader.js';
+import './modules/site-statement.js';
+import './modules/popup.js';
+
+//const ads = Array.from({ length: 10 }, (_, index) => getAd(index));
+
+//Для обхода ошибки линтера
+//ads.toString();
