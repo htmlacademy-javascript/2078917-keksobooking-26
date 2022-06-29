1ed11c81aab166fc9653efa18bf7ac359a425c04
Keks <keks@htmlacademy.ru>
2022-06-29 18:28:34 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,6 +1,5 @@
-import './modules/advert/get-advert-objects.js';
+import './modules/advert/get-advert-elements.js';
 import './modules/form-submit.js';
 import './modules/form-validation.js';
-import './modules/advert/get-advert-elements.js';
 import './modules/map-loader.js';
-import './modules/site-statement.js';
+import './modules/site-statements/activate-site.js';
